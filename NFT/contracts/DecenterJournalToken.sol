f754934825149e9e47d812598ef867c529991a16
Veno Gaube <veno.gaube111@gmail.com>
2022-04-14 21:59:49 +0200

    NFT - Updating contract adding new dependancies.

--- NFT/contracts/DecenterJournalToken.sol
+++ NFT/contracts/DecenterJournalToken.sol
@@ -1,20 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
-import "openzeppelin-solidity/contracts/access/Ownable.sol";
-import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
-import "openzeppelin-solidity/contracts/utils/Counters.sol";
+import "../node_modules/openzeppelin-solidity/contracts/access/Ownable.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/openzeppelin-solidity/contracts/utils/Counters.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/utils/Strings.sol";
+
 
 contract DecenterJournalToken is Ownable, ERC721 {
+    using Address for address payable;
     using Counters for Counters.Counter;
+    using Strings for uint256;
     Counters.Counter private _tokenIdCounter;
+
+    // Optional mapping for token URIs
+    mapping(uint256 => string) private _tokenURIs;
 
     struct Metadata {
         uint256 articleId;
+        address owner;
+
         string title;
         string author;
         string ipfs;
         uint256 articleData;
+        string articleImage;
     }
 
     mapping(uint256 => Metadata) decenterJournalTokenId;
@@ -32,14 +43,19 @@ contract DecenterJournalToken is Ownable, ERC721 {
         return _currentBaseURI;
     }
 
-    function mintAuthor(address authorAddress, string memory title, string memory author, string memory ipfs, uint256 articleData) external payable returns (uint256 tokenId){
-        require(msg.value == 0.01 ether, "claiming a token costs 0.01 ether");
-
+    function mintAuthor(string memory title, string memory author, string memory ipfs, uint256 articleData, string memory articleImage, string memory _tokenURI) external payable returns (uint256 tokenId){
+        require(msg.value == 0.0001 ether, "claiming a token costs 0.01 ether");
         _tokenIdCounter.increment();
         uint256 currentTokenId = _tokenIdCounter.current();
-        decenterJournalTokenId[currentTokenId] = Metadata(currentTokenId, title, author, ipfs, articleData);
-        _safeMint(authorAddress, currentTokenId);
+        decenterJournalTokenId[currentTokenId] = Metadata(currentTokenId, msg.sender, title, author, ipfs, articleData, articleImage);
+        _safeMint(msg.sender, currentTokenId);
+        _setTokenURI(currentTokenId, _tokenURI);
         return currentTokenId;
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual{
+        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
     }
 
     function get(uint256 tokenId) external view returns(string memory title, uint256 articleData) {
@@ -59,5 +75,22 @@ contract DecenterJournalToken is Ownable, ERC721 {
         require(_exists(tokenId), "token not minted");
         require(ownerOf(tokenId) == msg.sender, "only the owner of this token can change its title");
         decenterJournalTokenId[tokenId].title = title;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory){
+        require(_exists(tokenId), "ERC721 Metadata: URI query for nonexistent token.");
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        if(bytes(_tokenURI).length == 0){
+            return _tokenURI;
+        }
+
+        if(bytes(_tokenURI).length > 0){
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+
+        return string(abi.encodePacked(base, tokenId.toString()));
     }
 }
