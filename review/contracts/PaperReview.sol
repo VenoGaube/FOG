eace62934531ef57e7d15c6a15f95d3c2144fdc8
Rok Bosil <rb2600@student.uni-lj.si>
2022-05-11 00:27:48 +0200

    Paper review initial commit

---
+++ review/contracts/PaperReview.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.6.0 <0.9.0;
+
+contract PaperReview {
+    uint constant REQUIRED_VOTERS = 1;
+
+    enum VoteStatus { none, accept, reject }
+    enum ArticleStatus { none, inReview, finished }
+
+    struct Vote {
+        VoteStatus vote;
+        string reviewIpfsHash;
+    }
+
+    struct Abstract {
+        string ipfsHash;
+        ArticleStatus status;
+        mapping(address => Vote) votes;
+        uint8 votesNumber;
+    }
+
+    mapping(bytes32 => Article) submittedArticles;
+
+
+    // all actions after the article is submitted
+    event ArticleSubmitted(bytes32 articleId, string articleIpfsHash);
+    event ArticleAccepted(bytes32 articleId, string articleIpfsHash);
+    event ArticleRejected(bytes32 articleId, string articleIpfsHash);
+
+    // functions
+}
