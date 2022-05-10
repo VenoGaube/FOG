506a6d808e4ee5a57be6c3c70fd9110d5148563d
Nino <nino.brezac@gmail.com>
2022-05-11 01:17:41 +0200

    Added initial angular frontend

---
+++ frontend/app_public/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    
+  ],
+  providers: [],
+  bootstrap: [LoginComponent]
+})
+export class AppModule { }
