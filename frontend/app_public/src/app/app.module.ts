d8cfefcb5b5c027c02577bf04f4b047da09df0f2
phantomchicken <nino.brezac@gmail.com>
2022-06-05 23:58:41 +0200

    checkpoint metamask

--- frontend/app_public/src/app/app.module.ts
+++ frontend/app_public/src/app/app.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { TemplateComponent } from './template/template.component';
+import { RouterModule } from '@angular/router';
+import { ArticlesOverviewComponent } from './articles-overview/articles-overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    TemplateComponent,
+    ArticlesOverviewComponent
   ],
   imports: [
     BrowserModule,
-    
+    RouterModule.forRoot([{ path: '', component: TemplateComponent}, {path: 'articles', component: ArticlesOverviewComponent }]),
   ],
   providers: [],
-  bootstrap: [LoginComponent]
+  bootstrap: [TemplateComponent]
 })
 export class AppModule { }
---
+++ frontend/app_public/src/app/template/template.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import Web3 from 'web3';
+declare let require: any;
+
+declare global {
+  interface Window{
+    ethereum?:MetaMaskInpageProvider
+  }
+}
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+
+
+export class TemplateComponent implements OnInit {
+
+  constructor() { }
+  
+  vrniUporabnika = async (): Promise<string> => {
+    const ethereum = window.ethereum as MetaMaskInpageProvider;
+    if (typeof window.ethereum !== "undefined") {
+      // Poveži se na MetaMask
+      const racuni: any = await ethereum.request({method: "eth_requestAccounts"});
+      if (racuni != null) {
+        alert(racuni[0]);
+        return racuni[0];
+      } else return ""
+    } else return "";
+  }
+
+  ngOnInit(): void {
+    this.vrniUporabnika();
+    console.log("asdasd")
+  }
+
+}
