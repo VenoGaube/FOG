39a8cb8500aa5d6e0a3d133fc63e0bff4f0e6e10
Jure Vito Srovin <jure.vito@gmail.com>
2022-06-16 21:32:38 +0200

    Implement create file

--- ipfs/model/file.go
+++ ipfs/model/file.go
@@ -1,9 +1,14 @@
 package model
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
+	"time"
 )
 
 type IpfsResponse struct {
@@ -31,5 +36,50 @@ func (store *Store) GetFileByCID(cid string) (content []byte, err error) {
 }
 
 func (store *Store) CreateFile(content []byte) (cid string, err error) {
+
+	var b bytes.Buffer
+
+	w := multipart.NewWriter(&b)
+
+	var fw io.Writer
+	if fw, err = w.CreateFormFile("file", "publication.pdf"); err != nil {
+		return
+	}
+
+	if _, err = io.Copy(fw, bytes.NewReader(content)); err != nil {
+		return
+	}
+
+	if err = w.Close(); err != nil {
+		return
+	}
+
+	url := fmt.Sprintf("%s/api/v0/add", store.address)
+	req, err := http.NewRequest("POST", url, &b)
+	if err != nil {
+		return
+	}
+
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	var client *http.Client = &http.Client{Timeout: time.Duration(5) * time.Second}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	var ipfs IpfsResponse
+	if err = json.Unmarshal(body, &ipfs); err != nil {
+		return
+	}
+
+	cid = ipfs.Hash
+
 	return
 }
