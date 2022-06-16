f0298dd3d2ba59d85171b795273a30ffa551713e
zanmagerl <zan.magerl@hotmail.com>
2022-06-16 22:56:41 +0200

    Add basic connector to Ganache

--- paper-lifecycle/src/main/java/si/fri/fog/rest/TestFacade.java
+++ paper-lifecycle/src/main/java/si/fri/fog/rest/TestFacade.java
@@ -1,19 +1,30 @@
 package si.fri.fog.rest;
 
 import si.fri.fog.services.messaging.MailService;
+import si.fri.fog.services.web3.BlockchainEventListener;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 @Path("/test")
 public class TestFacade {
 
     private MailService mailService = new MailService();
 
+    private BlockchainEventListener blockchainEventListener = new BlockchainEventListener();
+
     @GET
     public Response getTest(){
         mailService.sendEmail("magerl.zan@gmail.com", "Test", "Lets hope it works");
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("/events")
+    public Response getEvents() throws IOException {
+        blockchainEventListener.checkEvents();
         return Response.ok().build();
     }
 }
