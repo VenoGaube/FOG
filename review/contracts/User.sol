75cad968b0e2996ca4378d40a487cbaede183bb5
Rok Bosil <rb2600@student.uni-lj.si>
2022-06-21 05:24:01 +0200

    Added smart contracts for review process

--- review/contracts/User.sol
+++ review/contracts/User.sol
@@ -1,13 +1,36 @@
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity ^0.4.8;
 
 contract User {
-    enum Role { none, author, reader, reviewer, editor }
+    enum UserType { none, reader, reviewer, author, editor }
 
-    mapping (address => Role) role;
+    // Maps user Ethereum addresses to internal IDs
+    mapping (address => uint32) userId;
+    // Maps user Ethereum addresses to their profiles on IPFS
+    mapping (address => string) userIpfsHash;
+    // Maps user Ethereum addresses to accounts types
+    mapping (address => UserType) userType;
 
-
-    function getUserRole(address userAddress) constant returns (Role) {
-        return role[userAddress];
+    function updateUserId(uint32 id) {
+        userId[msg.sender] = id;
     }
 
+    function retrieveUserId(address userAddress) constant returns (uint32) {
+        return userId[userAddress];
+    }
+
+    function updateUserData(string ipfsHash) {
+        userIpfsHash[msg.sender] = ipfsHash;
+    }
+
+    function retrieveUserData(address userAddress) constant returns (string) {
+        return userIpfsHash[userAddress];
+    }
+
+    function updateUserType(UserType newType) {
+        userType[msg.sender] = newType;
+    }
+
+    function retrieveUserType(address userAddress) constant returns (UserType) {
+        return userType[userAddress];
+    }
 }
