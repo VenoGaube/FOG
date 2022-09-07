3026ab37ee9ae7dd4b5423ac4a8fbb548797dd24
Rok Bosil <rb2600@student.uni-lj.si>
2022-09-07 07:37:53 +0200

    Review contract done

--- review/contracts/Review.sol
+++ review/contracts/Review.sol
@@ -1,26 +1,28 @@
-pragma solidity ^0.4.0;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.4.8;
 
 import "./User.sol";
 
 contract Review {
-    uint constant REQUIRED_REVIEWERS = 3;
+    int8 constant REQUIRED_REVIEWERS = 3;
     uint constant VOTING_PERIOD_DURATION = 45 days;
 
     // address of the user (could be a reviewer/editor/...)
     address public userContractAddress;
-    User userContract;
+//    User userContract;
+    enum UserType { none, reader, reviewer, author, editor }
+    mapping (address => UserType) userType;
 
     enum VoteStatus { none, accept, reject, revise }
-    enum ArticleStatus { none, waitingForReviewers, voting, revise, done}
+    enum ArticleStatus { none, waitingForReviewers, voting, revise, accepted, rejected}
 
     struct Vote {
-        VoteStatus vote;
+        bool voted;
         int8 grade;
 //        string feedbackIpfsHash;
     }
 
-    // should articles be stored in a seperate database (temporary database)
-    // if so then this is not needed
     struct Article {
         string ipfsHash;
         ArticleStatus status;
@@ -29,43 +31,95 @@ contract Review {
         address[] reviewers;
         mapping (address => bool) reviewers_bool;
         mapping(address => Vote) votes;
-        uint8 voteCount;
+        int8 voteCount;
 
-        address[] voteRequests;
-        uint32 collateralTotal;
-        mapping(address => uint256) requestTimes;
-        mapping(address => uint32) collateralAmounts;
-        mapping(address => bool) votingRights;
+//        address[] voteRequests;
+//        uint32 collateralTotal;
+//        mapping(address => uint256) requestTimes;
+//        mapping(address => uint32) collateralAmounts;
+//        mapping(address => bool) votingRights;
     }
 
     mapping(bytes32 => Article) submittedArticles;
 
     // events triggered by specific actions (article submitted, accepted/rejected)
-    event ArticleSubmitted(bytes32 articleId, string articleIpfsHash);
-    event VotingStarted(bytes32 articleId, string articleIpfsHash);
-    event ArticleAccepted(bytes32 articleId, string articleIpfsHash);
-    event ArticleRejected(bytes32 articleId, string articleIpfsHash);
-    event VotingPeriodExpired(bytes32 articleId);
+    event ArticleSubmitted(string event_name, bytes32 articleId, string articleIpfsHash);
+    event VotingStarted(string event_name, bytes32 articleId, string articleIpfsHash);
+    event ArticleAccepted(string event_name, bytes32 articleId, string articleIpfsHash);
+    event ArticleRejected(string event_name, bytes32 articleId, string articleIpfsHash);
+    event ArticleForRevision(string event_name, bytes32 articleId, string articleIpfsHAsh);
+    event VotingPeriodExpired(string event_name, bytes32 articleId);
 
-    constructor() {
-        userContractAddress = msg.sender();
+//    constructor() public {
+//        userContractAddress = msg.sender;
+//        userContract = User(userContractAddress);
+//    }
+
+    constructor(int editor) public {
+        userContractAddress = msg.sender;
+        if (editor == 0){
+            userType[msg.sender] = UserType.editor;
+        }
     }
 
-    function Review(){
-        userContract = User(userContractAddress);
-    }
 
-    // functions
-
-    function submitArticleForReview(string articleIpfsHash) {
-        //TODO: check if article is already in the submited articles and if it can be re-submitted
-
+    // functions for setting and retrieving the type of user
+    // can only be set by the editor role
+    function setAnyUserType(int t, address userAddress) public {
         require(
-            userContract.retrieveUserType(msg.sender) == User.UserType.author,
+            userType[msg.sender] == UserType.editor,
+            "Only an editor can change types"
+        );
+
+        if (t == 1){
+            userType[userAddress] = UserType.reader;
+        } else if (t == 2){
+            userType[userAddress] = UserType.reviewer;
+        } else if (t == 3){
+            userType[userAddress] = UserType.author;
+        } else if (t == 4){
+            userType[userAddress] = UserType.editor;
+        } else {
+            userType[userAddress] = UserType.none;
+        }
+    }
+
+    function setUserType(UserType newType, address userAddress) public {
+        require(
+            userType[msg.sender] == UserType.editor,
+            "Only an editor can change types"
+        );
+
+        userType[userAddress] = newType;
+    }
+
+    function getUserType(address userAddress) public view returns (UserType) {
+        return userType[userAddress];
+    }
+
+    function getArticleStatus(string memory articleIpfsHash) public view returns (ArticleStatus) {
+        bytes32 articleId = sha256(abi.encodePacked(articleIpfsHash));
+        Article storage article = submittedArticles[articleId];
+
+        return article.status;
+    }
+
+    function getAssignedReviewer(string memory articleIpfsHash, address a) public view returns (bool) {
+        bytes32 articleId = sha256(abi.encodePacked(articleIpfsHash));
+        Article storage article = submittedArticles[articleId];
+
+        return article.reviewers_bool[a];
+    }
+
+
+    // functions for review process
+    function submitArticleForReview(string memory articleIpfsHash) public {
+        require(
+            getUserType(msg.sender) == UserType.author,
             "Only an author can submit an article for a review."
         );
 
-        bytes articleId = sha3(articleIpfsHash);
+        bytes32 articleId = sha256(abi.encodePacked(articleIpfsHash));
 
         Article memory newArticle;
         newArticle.ipfsHash = articleIpfsHash;
@@ -75,17 +129,17 @@ contract Review {
 
         submittedArticles[articleId] = newArticle;
 
-        ArticleSubmitted(articleId, articleIpfsHash);
+        emit ArticleSubmitted('ArticleSubmitted', articleId, articleIpfsHash);
     }
 
-    function assignReviewers(string articleIpfsHash, address[] assigned_reviewers) {
+    function assignReviewers(string memory articleIpfsHash, address[] assigned_reviewers) public {
         require(
-            userContract.retrieveUserType(msg.sender) == User.UserType.editor,
+            getUserType(msg.sender) == UserType.editor,
             "Only an editor can assign reviewers."
         );
 
-        bytes articleId = sha3(articleIpfsHash);
-        Article article = submittedArticles[articleId];
+        bytes32 articleId = sha256(abi.encodePacked(articleIpfsHash));
+        Article storage article = submittedArticles[articleId];
 
         require(
             article.status == ArticleStatus.waitingForReviewers,
@@ -93,29 +147,33 @@ contract Review {
         );
 
         require(
-            assigned_reviewers.length == REQUIRED_REVIEWERS,
+            int8(assigned_reviewers.length) == REQUIRED_REVIEWERS,
             "There must be at least 3 reviewers per article."
         );
 
 //        article.reviewers_bool = assigned_reviewers;
         for (uint i = 0; i < assigned_reviewers.length; i++) {
+            // make assigned_reviewers type reviewer
+            // this is part of ID management
+            setUserType(UserType.reviewer, assigned_reviewers[i]);
+
             article.reviewers_bool[assigned_reviewers[i]] = true;
             article.reviewers = assigned_reviewers;
         }
 
         article.status = ArticleStatus.voting;
 
-        VotingStarted(articleId, article.ipfsHash);
+        emit VotingStarted('VotingStarted', articleId, article.ipfsHash);
     }
 
-    function vote(string articleIpfsHash, VoteStatus _vote, int8 _grade) {
+    function vote(string memory articleIpfsHash, int8 _grade) public {
         require(
-            userContract.retrieveUserType(msg.sender) == User.UserType.reviewer,
+            getUserType(msg.sender) == UserType.reviewer,
             "You must be a reviewer to review an article."
         );
 
-        bytes articleId = sha3(articleIpfsHash);
-        Article article = submittedArticles[articleId];
+        bytes32 articleId = sha256(abi.encodePacked(articleIpfsHash));
+        Article storage article = submittedArticles[articleId];
 
         require(
             article.reviewers_bool[msg.sender] == true,
@@ -123,24 +181,24 @@ contract Review {
         );
 
         require(
-            article.status == ArticleStatus.voting,
-            "You must be a reviewer assigned to this article to review the article."
+            article.status == ArticleStatus.voting || article.status == ArticleStatus.revise,
+            "The article must be in status voting or revise."
         );
 
         require(
-            article.votes[msg.sender].vote == VoteStatus.none,
+            article.votes[msg.sender].voted == false,
             "You already casted a vote for this article"
         );
 
         // check if the voting period has expired
         if (now > article.submissionTime + VOTING_PERIOD_DURATION) {
-            VotingPeriodExpired(articleId);
-            throw;
+            emit VotingPeriodExpired('VotingPeriodExpired', articleId);
+            assert(false);
         }
 
         // make a new vote
         Vote memory newVote;
-        newVote.vote = _vote;
+        newVote.voted = true;
         newVote.grade = _grade;
         article.votes[msg.sender] = newVote;
         article.voteCount++;
@@ -153,7 +211,7 @@ contract Review {
     }
 
     function makeDecision(bytes32 articleId) internal {
-        Article article = submittedArticles[articleId];
+        Article storage article = submittedArticles[articleId];
         VoteStatus decision;
 
         // calculate combined votes
@@ -187,23 +245,37 @@ contract Review {
                 decision = VoteStatus.accept;
             }
             else {
-                decision = VoteStatus.reject;
+                decision = VoteStatus.revise;
             }
         }
 
         // trigger events based on the final decision
         if (decision == VoteStatus.accept) {
-            article.status = ArticleStatus.done;
-            ArticleAccepted(articleId, article.ipfsHash);
+            article.status = ArticleStatus.accepted;
+            emit ArticleAccepted('ArticleAccepted', articleId, article.ipfsHash);
+        }
+        else if (decision == VoteStatus.revise) {
+            // the article can only be revised once, so it is rejected
+            if (article.status == ArticleStatus.revise) {
+                article.status = ArticleStatus.rejected;
+                emit ArticleRejected('ArticleRejected', articleId, article.ipfsHash);
+            }
+            // the article needs to be revised
+            else {
+                // reset the needed values
+                article.status = ArticleStatus.revise;
+                article.voteCount = 0;
+                for (uint j = 0; j < article.reviewers.length; j++) {
+                    article.votes[article.reviewers[j]].voted = false;
+                }
+                emit ArticleForRevision('ArticleForRevision', articleId, article.ipfsHash);
+            }
+        }
+        else if (decision == VoteStatus.reject) {
+            emit ArticleRejected('ArticleRejected', articleId, article.ipfsHash);
         }
         else {
-            if (article.status == ArticleStatus.revise) {
-                article.status = ArticleStatus.done;
-            }
-            else {
-                article.status = ArticleStatus.revise;
-            }
-            ArticleRejected(articleId, article.ipfsHash);
+            assert(false);
         }
 
     }
--- review/scripts/review.py
+++ review/scripts/review.py
@@ -1,5 +1,12 @@
-from brownie import Review, accounts
+from brownie import accounts, Review, User
+from scripts.listener_helper import start_listener, terminate_listener, start_listener_2
 
 
-def main():
-    return Review.deploy("Test Review", "TST", 18, 1e21, {'from': accounts[0]})
+def init():
+    # deploy the contract
+    r = Review.deploy(0, {'from': accounts[0]})
+
+    # start the oracle
+    p = start_listener(str(r.address), r.abi)
+
+    return r, p
